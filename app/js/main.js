605bf79861e79defda37bb2999de2cdc0b0ffbda
nataliiacat <nataliagricaj6@gmail.com>
2024-06-06 18:12:41 +0300

    added news

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,19 @@
 $(function () {
   $('.top-slider__inner').slick({
-    dots: false, 
+    dots: false,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 2500,
+    autoplaySpeed: 1000,
   })
+
+  $('.news-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    loop:false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+  });
 })
